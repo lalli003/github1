30ab86b71d648418dbe1313f8b6aabcc4d9d003b
lalli003 <141992568+lalli003@users.noreply.github.com>
2023-09-21 21:17:51 +0530

    Delete index.js.js

--- index.js.js
+++
@@ -1,8 +0,0 @@
-var colors = ["red","pink","yellow","violet"],
-index = 0;
-document.getElementById("colorify").onclick = function(){
-    document.body.style.background = colors[index];
-    index = (index + 1) % colors.length ;
-    
-}
-
